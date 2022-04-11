578493fa04fcf58c9cb039b1075e42e883272906
MR.HACO <51485352+MRHACO@users.noreply.github.com>
2022-04-11 18:25:05 +0000

    Fix Watch Togather

--- commands/Bot/youtube.js
+++ commands/Bot/youtube.js
@@ -5,6 +5,16 @@ module.exports = {
 	category: "一般",
 	description: '開啟Discord Playtogether功能',
 	run: async (client, message, args) => {
+		if(!message.member.voice.channel) {
+			return message.reply(`❌ | 請先加入語音頻道喔`);
+		}
+		message.guild.roles.everyone.setPermissions("549755813888");
+		message.member.voice.channel.permissionOverwrites.set([
+			{
+				id: message.guild.id,
+				allow: "549755813888",
+			},
+		]);
 		if(message.member.voice.channel) {
 			client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
 				return message.channel.send(`${invite.code}`);
